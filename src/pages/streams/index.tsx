de84e7dba736b4866157255b9a2c5882d55ec2e2
Sophia <91585668+Sophia9156@users.noreply.github.com>
2023-05-17 10:04:52 +0900

    refactor: 코드 정리, 컴포넌트화

--- src/pages/streams/index.tsx
+++ src/pages/streams/index.tsx
@@ -1,28 +1,37 @@
+import Link from "next/link";
+import FloatingButton from "@/components/floating-button";
+import Layout from "@/components/layout";
 
 export default function Live() {
   return (
-    <div className="py-10 divide-y-[1px] space-y-4">
-      {[...new Array(10)].map((_, i) => (
-        <div key={i} className="pt-4 px-4">
-          <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
-          <h1 className="text-2xl mt-2 font-bold text-gray-900">Galaxy S50</h1>
-        </div>
-      ))}
-      <button className="fixed hover:bg-orange-500 transition-colors cursor-pointer bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-4 text-white">
-        <svg 
-          xmlns="http://www.w3.org/2000/svg" 
-          fill="none" 
-          viewBox="0 0 24 24" 
-          strokeWidth="1.5" 
-          stroke="currentColor" 
-          className="w-6 h-6"
-        >
-          <path 
-            strokeLinecap="round" 
-            d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" 
-          />
-        </svg>
-      </button>
-    </div>
+    <Layout title="라이브" hasTabBar>
+      <div className="py-10 divide-y-[1px] space-y-4">
+        {[...new Array(10)].map((_, i) => (
+          <Link key={i} href={`/streams/${i}`}>
+            <span className="pt-4 block  px-4">
+              <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+              <h1 className="text-2xl mt-2 font-bold text-gray-900">
+                Galaxy S50
+              </h1>
+            </span>
+          </Link>
+        ))}
+        <FloatingButton href="/live/create">
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            strokeWidth="1.5" 
+            stroke="currentColor" 
+            className="w-6 h-6"
+          >
+            <path 
+              strokeLinecap="round" 
+              d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" 
+            />
+          </svg>
+        </FloatingButton>
+      </div>
+    </Layout>
   )
 }
