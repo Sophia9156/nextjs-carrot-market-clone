64f5728468a1e63c44ed1346d0ef6dcb14ee2992
Sophia9156 <suhyun9156@gmail.com>
2023-06-25 13:10:52 +0900

    [#11] feat: added profile handler

--- src/pages/api/users/me.ts
+++ src/pages/api/users/me.ts
@@ -15,22 +15,16 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { token } = req.body;
-  const exists = await client.token.findUnique({
-    where: {
-      payload: token,
-    },
-    include: { user: true },
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id },
   });
-  if (!exists) return res.status(404).end();
-  req.session.user = {
-    id: exists.userId,
-  };
-  await req.session.save();
-  res.status(200).end();
+  res.json({
+    ok: true,
+    profile,
+  });
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
+export default withIronSessionApiRoute(withHandler("GET", handler), {
   cookieName: "carrotsession",
   password: process.env.IRON_SESSION_PASSWORD!,
 });
