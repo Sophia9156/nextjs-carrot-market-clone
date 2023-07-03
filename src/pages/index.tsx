badb88b66275e2f5fa4b13ddc6ae4797fcb48d83
Sophia9156 <suhyun9156@gmail.com>
2023-07-03 11:16:29 +0900

    [#18] feat: added useUser Hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import Layout from "@/components/layout";
 import FloatingButton from "@/components/floating-button";
 import Item from "@/components/item";
+import useUser from "@/libs/client/useUser";
+import Head from "next/head";
 
 export default function Home() {
+  const user = useUser();
+  console.log(user);
   return (
-    <Layout title="홈" hasTabBar>
+    <Layout
+      title="홈"
+      hasTabBar>
+      <Head>
+        <title>Home</title>
+      </Head>
       <div className="flex px-4 flex-col space-y-5 py-10">
         {[...new Array(10)].map((_, i) => (
-          <Item 
-            id={i} 
-            key={i} 
+          <Item
+            id={i}
+            key={i}
             title="iPhone 14"
             price={99}
             comments={1}
@@ -23,8 +32,7 @@ export default function Home() {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            aria-hidden="true"
-          >
+            aria-hidden="true">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
