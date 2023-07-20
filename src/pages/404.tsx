fceaceb7ac49ae6f3cf64122c66cdeccacdcf737
Sophia9156 <suhyun9156@gmail.com>
2023-07-20 11:05:53 +0900

    [#50] feat: added get post detail feature

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import { useRouter } from "next/router";
+
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}>
+      <p style={{ fontSize: 36, fontWeight: "bold" }}>404</p>
+      <p style={{ fontSize: 16 }}>Page Not Found</p>
+    </div>
+  );
+}
