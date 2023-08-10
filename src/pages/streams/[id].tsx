c72bdb681ec62f7a3b5e84f0de468504d39da684
Sophia9156 <suhyun9156@gmail.com>
2023-08-10 12:52:43 +0900

    [#84] feat: added stream messages mutate and refresh function

--- src/pages/streams/[id].tsx
+++ src/pages/streams/[id].tsx
@@ -35,7 +35,10 @@ export default function Stream() {
   const { user } = useUser();
   const { register, handleSubmit, reset } = useForm<MessageForm>();
   const { data, mutate } = useSWR<StreamResponse>(
-    router.query.id ? `/api/streams/${router.query.id}` : null
+    router.query.id ? `/api/streams/${router.query.id}` : null,
+    {
+      refreshInterval: 1000,
+    }
   );
   const [sendMessage, { loading, data: sendMessageData }] = useMutation(
     `/api/streams/${router.query.id}/messages`
@@ -43,13 +46,23 @@ export default function Stream() {
   const onValid = (form: MessageForm) => {
     if (loading) return;
     reset();
+    mutate(
+      (prev) =>
+        prev &&
+        ({
+          ...prev,
+          stream: {
+            ...prev.stream,
+            messages: [
+              ...prev.stream.messages,
+              { id: Date.now(), message: form.message, user: { ...user } },
+            ],
+          },
+        } as any),
+      false
+    );
     sendMessage(form);
   };
-  useEffect(() => {
-    if (sendMessageData && sendMessageData.ok) {
-      mutate();
-    }
-  }, [mutate, sendMessageData]);
 
   return (
     <Layout canGoBack>
