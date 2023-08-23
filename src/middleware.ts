c7a00c0fb6d7e8f4b14f33c5b49963b5a6f6bd6c
Sophia9156 <suhyun9156@gmail.com>
2023-08-23 12:35:41 +0900

    [#109] feat: added auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,22 @@
-import { NextRequest, NextFetchEvent, userAgent } from "next/server";
+import {
+  NextRequest,
+  NextResponse,
+  NextFetchEvent,
+  userAgent,
+} from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log(userAgent(req));
-
-  if (req.nextUrl.pathname.startsWith("/chats")) {
-    console.log(`this is chats ONLY middleware!`);
+  if (userAgent(req).isBot) {
+    return new Response("I hope you are not a bot.", { status: 403 });
   }
+  if (!req.cookies.has("carrotsession") && !req.url.includes("/enter")) {
+    req.nextUrl.searchParams.set("from", req.nextUrl.pathname);
+    req.nextUrl.pathname = "/enter";
+    return NextResponse.redirect(req.nextUrl);
+  }
+  return NextResponse.json({ ok: true });
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|favicon.ico).*)"],
+};
